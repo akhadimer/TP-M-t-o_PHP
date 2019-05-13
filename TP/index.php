ed0c574df4e3c0a8e1097931a52a34b66d43abac
Thomas Dumont <thomas.dumont@ynov.com>
2019-05-13 23:11:26 +0200

    update

---
+++ TP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>Météo</title>
+</head>
+
+<body>
+<p>Aujourd'hui nous sommes le <?php echo date('d/m/Y'); ?>.</p>
+<a href="bordeaux.php">Météo à Bordeaux</a>
+<a href="paris.php">Météo à Paris</a>
+</body>
+
+</html>
---
+++ TP/paris.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Météo Paris</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+  <a href="index.php">Accueil</a>
+  <br>
+  <?php $json = file_get_contents('https://www.prevision-meteo.ch/services/json/paris'); ?>
+  <?php $json = json_decode($json); ?>
+  <div class="container">
+    <h2>Météo <?php echo $json->city_info->name; ?></h2>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Température minimale</th>
+          <th>Température maximale</th>
+          <th>Temps</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>J+0</td>
+          <td>
+            <p><?php echo $json->fcst_day_0->tmin; ?>°C</p>
+          </td>
+          <td><p><?php echo $json->fcst_day_0->tmax; ?>°C</td>
+          <td><img src="<?php echo $json->fcst_day_0->icon; ?>" /></td>
+        </tr>
+        <tr>
+          <td>J+1</td>
+          <td>
+            <p><?php echo $json->fcst_day_1->tmin; ?>°C</p>
+          </td>
+          <td><p><?php echo $json->fcst_day_1->tmax; ?>°C</td>
+          <td><img src="<?php echo $json->fcst_day_1->icon; ?>" /></td>
+        </tr>
+        <tr>
+          <td>J+2</td>
+          <td>
+            <p><?php echo $json->fcst_day_2->tmin; ?>°C</p>
+          </td>
+          <td><p><?php echo $json->fcst_day_2->tmax; ?>°C</td>
+          <td><img src="<?php echo $json->fcst_day_2->icon; ?>" /></td>
+        </tr>
+        <tr>
+          <td>J+3</td>
+          <td>
+            <p><?php echo $json->fcst_day_3->tmin; ?>°C</p>
+          </td>
+          <td><p><?php echo $json->fcst_day_3->tmax; ?>°C</td>
+          <td><img src="<?php echo $json->fcst_day_3->icon; ?>" /></td>
+        </tr>
+        <tr>
+          <td>J+4</td>
+          <td>
+            <p><?php echo $json->fcst_day_4->tmin; ?>°C</p>
+          </td>
+          <td><p><?php echo $json->fcst_day_4->tmax; ?>°C</td>
+          <td><img src="<?php echo $json->fcst_day_4->icon; ?>" /></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</body>
+
+</html>
